03a4185f1ece6eed8a3abd51073fb97bd913685a
zacharytabler <zachary.tabler@gmail.com>
2021-08-29 13:05:26 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+let saveEl = document.getElementById("save-el");
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+  console.log(count);
+}
+
+function save() {
+  let countStr = count + "-";
+  saveEl.textContent += countStr;
+  count = 0;
+  countEl.textContent = 0;
+}
